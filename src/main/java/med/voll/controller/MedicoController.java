ecb99b6e8e9d6c31922a2dcb2b1fb3fe85966612
gustavosouzza <gustaavo.goncaalves@gmail.com>
2025-09-08 23:37:32 -0300

    feat(medicos): adicionar endpoint DELETE para desativar médico e listar apenas ativos

--- src/main/java/med/voll/controller/MedicoController.java
+++ src/main/java/med/voll/controller/MedicoController.java
@@ -9,7 +9,6 @@ import org.springframework.data.web.PageableDefault;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.bind.annotation.*;
 
-import java.util.List;
 
 @RestController
 @RequestMapping ("/medicos")
@@ -26,7 +25,7 @@ public class MedicoController {
 
     @GetMapping
     public Page<DadosListagemMedico> listar(@PageableDefault(size = 10, sort = {"nome"}) Pageable paginacao) {
-        return repository.findAll(paginacao).map(DadosListagemMedico::new);
+        return repository.findAllByAtivoTrue(paginacao).map(DadosListagemMedico::new);
     }
 
     @PutMapping
@@ -35,4 +34,14 @@ public class MedicoController {
         var medico = repository.getReferenceById(dados.id());
         medico.atualizarInformacoes(dados);
     }
+
+    @DeleteMapping ("/{id}")
+    @Transactional
+    public void excluir(@PathVariable Long id) {
+        var medico = repository.getReferenceById(id);
+        medico.excluir();
+    }
+
+
+
 }
