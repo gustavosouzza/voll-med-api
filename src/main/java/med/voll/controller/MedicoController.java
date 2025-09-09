46e0a4c3618abf49f8d227c7175a69f4088a8cbc
gustavosouzza <gustaavo.goncaalves@gmail.com>
2025-09-08 21:13:43 -0300

    feat(medicos): adicionar paginação à listagem de médicos

--- src/main/java/med/voll/controller/MedicoController.java
+++ src/main/java/med/voll/controller/MedicoController.java
@@ -6,6 +6,9 @@ import med.voll.medico.DadosListagemMedico;
 import med.voll.medico.Medico;
 import med.voll.medico.MedicoRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.web.PageableDefault;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.bind.annotation.*;
 
@@ -25,7 +28,7 @@ public class MedicoController {
     }
 
     @GetMapping
-    public List<DadosListagemMedico> listar() {
-       return repository.findAll().stream().map(DadosListagemMedico::new).toList();
+    public Page<DadosListagemMedico> listar(@PageableDefault(size = 10 , sort = {"nome"}) Pageable paginacao) {
+       return repository.findAll(paginacao).map(DadosListagemMedico::new);
     }
 }
