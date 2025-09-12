07556457bcbc8af8e13bf586a6d1dd7a6f01d1cd
gustavosouzza <gustaavo.goncaalves@gmail.com>
2025-09-12 02:26:40 -0300

    feat(security): implementar autenticação JWT com filtro customizado

--- src/main/java/med/voll/infra/security/TokenService.java
+++ src/main/java/med/voll/infra/security/TokenService.java
@@ -1,8 +1,10 @@
 package med.voll.infra.security;
 
 import com.auth0.jwt.JWT;
+import com.auth0.jwt.JWTVerifier;
 import com.auth0.jwt.algorithms.Algorithm;
 import com.auth0.jwt.exceptions.JWTCreationException;
+import com.auth0.jwt.exceptions.JWTVerificationException;
 import med.voll.domain.usuario.Usuario;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
@@ -25,6 +27,19 @@ public class TokenService  {
                     .sign(algoritmo);
         } catch (JWTCreationException exception) {
             throw new RuntimeException("Erro ao gerar token JWT", exception);
+        }
+    }
+
+    public String getSubject(String tokenJWT) {
+        try {
+            var algoritmo = Algorithm.HMAC256(secret);
+            return JWT.require(algoritmo)
+                    .withIssuer("API Voll.med")
+                    .build()
+                    .verify(tokenJWT)
+                    .getSubject();
+        } catch (JWTVerificationException exception) {
+            throw new RuntimeException("Token JWT inválido ou expirado!");
         }
     }
 
