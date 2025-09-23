4176eb30e00a5cc2b7b8d26d50bdf408d324c121
gustavosouzza <gustaavo.goncaalves@gmail.com>
2025-09-23 10:08:58 -0300

    feat(dto): adicionar DTOs para agendamento e detalhamento de consultas

---
+++ src/main/java/med/voll/domain/consulta/DadosDetalhamentoConsulta.java
@@ -0,0 +1,11 @@
+package med.voll.domain.consulta;
+
+import java.time.LocalDateTime;
+
+public record DadosDetalhamentoConsulta(
+        Long id,
+        Long idMedico,
+        Long idPaciente,
+        LocalDateTime data
+) {
+}
