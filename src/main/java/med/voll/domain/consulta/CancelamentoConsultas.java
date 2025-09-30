5f1f81898be37133c60a00d98b64515c82b627b9
gustavosouzza <gustaavo.goncaalves@gmail.com>
2025-09-30 20:15:32 -0300

    feat: adicionar serviço de cancelamento de consultas

--- src/main/java/med/voll/domain/consulta/CancelamentoConsultas.java
+++ src/main/java/med/voll/domain/consulta/CancelamentoConsultas.java
@@ -1,4 +1,20 @@
 package med.voll.domain.consulta;
 
+import med.voll.domain.ValidacaoException;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
 public class CancelamentoConsultas {
+
+    @Autowired
+    private ConsultaRepository consultaRepository;
+
+    public void cancelar(DadosCancelamentoConsulta dados) {
+       if (!consultaRepository.existsById(dados.idConsulta())) {
+           throw new ValidacaoException("Id da consulta informado não existe!");
+       }
+       var consulta = consultaRepository.getReferenceById(dados.idConsulta());
+       consulta.cancelar(dados.motivo());
+    }
 }
