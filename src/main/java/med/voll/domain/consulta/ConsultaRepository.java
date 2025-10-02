9e98350685e68b63601d8ae8e4be9bc051a019c2
gustavosouzza <gustaavo.goncaalves@gmail.com>
2025-10-02 20:25:40 -0300

    feat: adicionar ConsultaRepository com métodos de verificação de agendamento

--- src/main/java/med/voll/domain/consulta/ConsultaRepository.java
+++ src/main/java/med/voll/domain/consulta/ConsultaRepository.java
@@ -1,6 +1,12 @@
 package med.voll.domain.consulta;
 
+import java.time.LocalDateTime;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface ConsultaRepository extends JpaRepository<Consulta, Long> {
+
+  boolean existsByMedicoIdAndDataAndMotivoCancelamentoIsNull(Long idMedico, LocalDateTime data);
+
+  Boolean existsByPacienteIdAndDataBetween(
+      Long idPaciente, LocalDateTime primeiroHorario, LocalDateTime ultimoHorario);
 }
