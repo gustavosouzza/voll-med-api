69b40f3cbed453c89c9eef18731773d9e65e4397
gustavosouzza <gustaavo.goncaalves@gmail.com>
2025-10-02 20:24:07 -0300

    feat: adicionar entidade Consulta com atributos e mapeamento JPA

--- src/main/java/med/voll/domain/consulta/Consulta.java
+++ src/main/java/med/voll/domain/consulta/Consulta.java
@@ -1,39 +1,37 @@
 package med.voll.domain.consulta;
 
 import jakarta.persistence.*;
-import jakarta.validation.constraints.NotNull;
+import java.time.LocalDateTime;
 import lombok.*;
 import med.voll.domain.medico.Medico;
 import med.voll.domain.paciente.Paciente;
 
-import java.time.LocalDateTime;
-
-@Table (name = "consultas")
-@Entity (name = "Consulta")
+@Table(name = "consultas")
+@Entity(name = "Consulta")
 @Getter
 @AllArgsConstructor
 @NoArgsConstructor
 @EqualsAndHashCode(of = "id")
 public class Consulta {
-    @Id
-    @GeneratedValue (strategy = jakarta.persistence.GenerationType.IDENTITY)
-    private Long id;
+  @Id
+  @GeneratedValue(strategy = jakarta.persistence.GenerationType.IDENTITY)
+  private Long id;
 
-    @ManyToOne
-    @JoinColumn (name = "id_medico")
-    private Medico medico;
+  @ManyToOne
+  @JoinColumn(name = "medico_id")
+  private Medico medico;
 
-    @ManyToOne
-    @JoinColumn (name = "id_paciente")
-    private Paciente paciente;
+  @ManyToOne
+  @JoinColumn(name = "paciente_id")
+  private Paciente paciente;
 
-    private LocalDateTime data;
+  private LocalDateTime data;
 
-    @Column(name = "motivo_cancelamento")
-    @Enumerated(EnumType.STRING)
-    private MotivoCancelamento motivoCancelamento;
+  @Column(name = "motivo_cancelamento")
+  @Enumerated(EnumType.STRING)
+  private MotivoCancelamento motivoCancelamento;
 
-    public void cancelar(MotivoCancelamento motivo) {
-        this.motivoCancelamento = motivo;
-    }
+  public void cancelar(MotivoCancelamento motivo) {
+    this.motivoCancelamento = motivo;
+  }
 }
