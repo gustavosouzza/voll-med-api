a051396084fea0d6a4434d8c5a1edcad1923ceb8
gustavosouzza <gustaavo.goncaalves@gmail.com>
2025-10-02 20:17:01 -0300

    feat: adicionar Controller para gerenciar consultas

--- src/main/java/med/voll/controller/ConsultaController.java
+++ src/main/java/med/voll/controller/ConsultaController.java
@@ -5,28 +5,27 @@ import jakarta.validation.Valid;
 import med.voll.domain.consulta.AgendaDeConsultas;
 import med.voll.domain.consulta.DadosAgendamentoConsulta;
 import med.voll.domain.consulta.DadosCancelamentoConsulta;
-import med.voll.domain.consulta.DadosDetalhamentoConsulta;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
-@RequestMapping ("/consultas")
+@RequestMapping("/consultas")
 public class ConsultaController {
 
-    @Autowired
-    private AgendaDeConsultas agenda;
-    @PostMapping
-    @Transactional
-    public ResponseEntity agendar(@RequestBody @Valid DadosAgendamentoConsulta dados) {
-        agenda.agendar(dados);
-        return ResponseEntity.ok(new DadosDetalhamentoConsulta(null, null, null , null    ) );
-    }
+  @Autowired private AgendaDeConsultas agenda;
 
-    @DeleteMapping
-    @Transactional
-    public ResponseEntity cancelar(@RequestBody @Valid DadosCancelamentoConsulta dados) {
-        agenda.cancelar(dados);
-        return ResponseEntity.noContent().build();
-    }
+  @PostMapping
+  @Transactional
+  public ResponseEntity agendar(@RequestBody @Valid DadosAgendamentoConsulta dados) {
+    var dto = agenda.agendar(dados);
+    return ResponseEntity.ok(dto);
+  }
+
+  @DeleteMapping
+  @Transactional
+  public ResponseEntity cancelar(@RequestBody @Valid DadosCancelamentoConsulta dados) {
+    agenda.cancelar(dados);
+    return ResponseEntity.noContent().build();
+  }
 }
